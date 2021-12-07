c709f5ecee77544487c71a33fafaf766becafd18
Egyeom <ug4306@gmail.com>
2021-12-08 00:06:17 +0900

    BinarySearch using Recursive

--- code/DataStructure/BinarySearch.cpp
+++ code/DataStructure/BinarySearch.cpp
@@ -3,7 +3,6 @@
 
 int BinarySearch(std::vector<int> arr, int key)
 {
-    std::sort(arr.begin(), arr.end());
 
     int left = 0;
     int right = arr.size();
@@ -15,6 +14,7 @@ int BinarySearch(std::vector<int> arr, int key)
         {
             left = middle +1;
         }
+    
         else if(arr[middle] > key)
         {
             right = middle -1;
@@ -27,17 +27,41 @@ int BinarySearch(std::vector<int> arr, int key)
     return -1;
 }
 
+int BinarySearch(std::vector<int>& arr, int left, int right,int key)
+{
+    if(left > right) return -1;
+
+    int middle = (left + right)/2;
+    
+    if(key == arr[middle])
+    {
+        return middle;
+    }
+    else if(key < arr[middle])
+    {
+        return BinarySearch(arr, left, middle-1, key);
+    }
+    else if(key > arr[middle])
+    {
+        return BinarySearch(arr, left+1, right, key);
+    }
+
+}
+
 int main()
 {
     std::vector<int> v = {3,5,9,7,10,2};
+    std::sort(v.begin(), v.end());
 
     int key = 9;
-
+    int left = 0;
+    int right = v.size() -1 ;
     int index = BinarySearch(v,key);
+    int indexbyReCursive = BinarySearch(v,left,right,key);
 
     if(index == -1)
     {
         printf("num : %d is not in array\n", key);
     }
-    else printf("num : %d find ! index = %d\n", key, index);
+    else printf("num : %d find ! index = %d\t indexbyReCursive = %d\n", key, index, indexbyReCursive);
 }
