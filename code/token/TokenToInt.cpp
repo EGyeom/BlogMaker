da097417b989043a1e838fcf6f7666f5793db1e6
egyeom <ug4306@gmail.com>
2022-01-26 10:24:14 +0900

    Token process

--- code/token/TokenToInt.cpp
+++ code/token/TokenToInt.cpp
@@ -12,6 +12,7 @@ int main()
     char num_buff[256] = {0};
 
     int fd = open("save.txt", O_RDONLY);
+    if(fd < 0) return 0;
 
     char * buff = new char[filesize];
 
@@ -21,7 +22,7 @@ int main()
 
     fd = open("test.txt", O_RDWR|O_CREAT|O_TRUNC, 00777);
 
-    for(int i =0; i < filesize; i++)
+    for(int i =0; i < filesize-1; i++)
     {
         int num = (int)(uint8_t)*(buff+i);
         sprintf(num_buff,"%d\n",num);
